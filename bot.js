58b6b83d638c81429d29b5948b505fbe20f02645
cerww <bonsly132@hotmail.com>
2017-02-11 01:44:12 -0800

    improved trivia

--- bot.js
+++ bot.js
@@ -45,7 +45,7 @@ function slots(){
 	for(i in temp){
 		if(temp[i]>1){
 			//toSend+="you won"+temp[i].toString()+" times";
-			o=" you won "+temp[i].toString()+" times";
+			o=" you got "+temp[i].toString()+"/3 matches";
 			break;
 		}
 	}if(o==""){
@@ -53,29 +53,53 @@ function slots(){
 	}//msg.message.channel.sendMessage(toSend+o);
 	return toSend+o;
 }
-function triviaQ(){
-	var qID = 0;
-	this.question = ["hi"][qID];
-	this.answer = ["b"][qID];
-}
-function triviaGame(maxQs){
+
+function triviaGame(maxQs,channel){
+	function triviaQ(){
+		var qID = 0;
+		this.question = ["how many cats are there"][qID];
+		this.answer = ["a lot"][qID];
+	}
 	this.currentQ = new triviaQ();
 	this.points = [];
 	this.maxQs = maxQs;
+	//console.log(maxQs);
+	this.channel = channel;
+	this.time = 0;
+	this.update = ()=>{
+		++this.time;
+		if(this.time>=20){
+			this.currentQ = new triviaQ();
+			this.channel.sendMessage("times up!\n"+"**"+this.currentQ.question+"**");
+			this.time = 0;
+		}
+	};
+	this.newQuestion = ()=>{
+		this.currentQ = new triviaQ();
+		this.time = 0;
+	};
 }
 var triviaGames = [];
 client.Dispatcher.on("MESSAGE_CREATE", msg => {
+  //check if ther is a trivia game in current ch
   if(triviaGames[msg.message.channel.id]!=undefined){
-    if(msg.message.content==triviaGames[msg.message.channel.id].currentQ.answer){
-	  msg.message.channel.sendMessage(msg.message.author.username+" won, answer was "+msg.message.content);
+    if(msg.message.content==triviaGames[msg.message.channel.id].currentQ.answer){//checks if its the right ans
+	  msg.message.channel.sendMessage(msg.message.author.username+" won, answer was **"+msg.message.content+"**");
 	  --triviaGames[msg.message.channel.id].maxQs;
 	  if(msg.message.author.username in triviaGames[msg.message.channel.id].points){
 		++triviaGames[msg.message.channel.id].points[msg.message.author.username];
-	  }
-	  if(!triviaGames[msg.message.channel.id].maxQs){
-		triviaGames[msg.message.channel.id] = undefined;
 	  }else{
-		triviaGames[msg.message.channel.id].currentQ = new triviaQ();
+		triviaGames[msg.message.channel.id].points[msg.message.author.username]=1;
+	  }//end trivia if maxQs == 0
+	  if(triviaGames[msg.message.channel.id].maxQs<=0||Number.isNaN(triviaGames[msg.message.channel.id].maxQs)){
+	    var a = "";
+		for(i in triviaGames[msg.message.channel.id].points){
+		  a+="` "+i+"`"+":"+triviaGames[msg.message.channel.id].points[i].toString()+"\n";
+		}
+		triviaGames[msg.message.channel.id] = undefined;
+		msg.message.channel.sendMessage(a);
+	  }else{
+		triviaGames[msg.message.channel.id].newQuestion();// = new triviaQ();
 		msg.message.channel.sendMessage("**"+triviaGames[msg.message.channel.id].currentQ.question+"**");
 	  }
 	}
@@ -90,11 +114,33 @@ client.Dispatcher.on("MESSAGE_CREATE", msg => {
 	}else if(args[0]=="%pong"){
 		msg.message.channel.sendMessage("pong "+msg.message.author.username);
 	}else if(args[0]=="%triv"){
-		triviaGames[msg.message.channel.id] = new triviaGame(2);
-		msg.message.channel.sendMessage("trivia game started\n"+"**"+triviaGames[msg.message.channel.id].currentQ.question+"**");
+	  if(triviaGames[msg.message.channel.id]==undefined){
+	    try{
+	      //a = 1/eval(args[1]);
+	  	  triviaGames[msg.message.channel.id] = new triviaGame(Math.min(eval(args[1]),100),msg.message.channel);
+	    }catch(e){
+		//console.log(e);
+  	      triviaGames[msg.message.channel.id] = new triviaGame(2,msg.message.channel);
+	    }
+	    msg.message.channel.sendMessage("trivia game started\n"+"**"+triviaGames[msg.message.channel.id].currentQ.question+"**");
+	  }else{
+		msg.message.channel.sendMessage("trivia game already started")
+	  }
+	}else if(args[0]=="%trivq"){
+		if(triviaGames[msg.message.channel.id]!=undefined){
+			triviaGames[msg.message.channel.id]= undefined;
+			msg.message.channel.sendMessage("trivia game over");
+		}else{
+			msg.message.channel.sendMessage("no trivia game on this channel");
+		}
 	}
 	//let firstSpace = e.message.content.indexOf(" ");
   }//for(var i = 0;i<1000000;++i){}
 });
-
+setInterval(function(){
+	for(i in triviaGames){
+		if(triviaGames[i] !== undefined)
+			triviaGames[i].update();
+	}
+},1000);
 //https://discordapp.com/oauth2/authorize?&client_id=267193568655835136&scope=bot
